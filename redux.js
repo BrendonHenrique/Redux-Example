1de995e43219b253ec5679e98bf3deb66180228f
BrendonHenrique <33273035+BrendonHenrique@users.noreply.github.com>
2019-09-29 03:15:57 -0300

    Update redux.js

--- redux.js
+++ redux.js
@@ -1,3 +1,4 @@
 const personController =  require('./Controllers/person')
 const person = new personController();
+// After controller has created you can use all that reducers implemented as methods.
 console.log(person.subscribe())
